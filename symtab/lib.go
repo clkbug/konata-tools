312cf877f6d4e8c6cba95bfb0eae50564062db45
clkbug <mail@clkbug.net>
2024-05-30 21:53:41 +0900

    add symtab

---
+++ symtab/lib.go
@@ -0,0 +1,114 @@
+package symtab
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type SymType int
+
+const (
+	SymNoType SymType = iota
+	SymFunc
+	SymObj
+	SymSection
+	SymFile
+)
+
+type Symbol struct {
+	Name string
+	Addr uint64
+	Size uint64
+	Type SymType
+}
+
+type SymbolTable []Symbol
+
+func ParseSymbolTableFile(filename string) (SymbolTable, error) {
+	fp, err := os.Open(filename)
+	if err != nil {
+		return SymbolTable{}, err
+	}
+
+	st := SymbolTable{}
+	buf := bufio.NewScanner(fp)
+	i := 0
+	for buf.Scan() {
+		i++
+		l := buf.Text()
+		if i <= 3 {
+			continue
+		}
+		l = strings.TrimSpace(l)
+		for range 10 {
+			l = strings.Replace(l, "  ", " ", -1)
+		}
+		w := strings.Split(l, " ")
+
+		addr, err := strconv.ParseUint(w[1], 16, 64)
+		if err != nil {
+			return SymbolTable{}, fmt.Errorf("failed to ParseUint(%s) in %s:%d '%s'", w[1], filename, i, l)
+		}
+		size, err := strconv.ParseUint(w[2], 16, 64)
+		if err != nil {
+			return SymbolTable{}, fmt.Errorf("failed to ParseUint(%s) in %s:%d '%s'", w[1], filename, i, l)
+		}
+		var t SymType
+		switch w[3] {
+		case "NOTYPE":
+			t = SymNoType
+		case "FILE":
+			t = SymFile
+		case "SECTION":
+			t = SymSection
+		case "FUNC":
+			t = SymFunc
+		case "OBJECT":
+			t = SymObj
+		default:
+			return SymbolTable{}, fmt.Errorf("unknown SymbolType '%s'", w[3])
+		}
+		if len(w) == 7 {
+			w = append(w, "")
+		}
+		s := Symbol{
+			Addr: addr,
+			Size: size,
+			Type: t,
+			Name: w[7],
+		}
+		st = append(st, s)
+	}
+	return st, nil
+}
+
+func (st SymbolTable) Sort() {
+	sort.Slice(st, func(i, j int) bool { return st[i].Addr < st[j].Addr })
+}
+
+func (st SymbolTable) Search(addr uint64) (Symbol, bool) {
+	low := 0
+	high := len(st) - 1
+	if addr < st[low].Addr || st[high].Addr+st[high].Size <= addr {
+		return Symbol{}, false
+	}
+	if st[high].Addr <= addr {
+		return st[high], true
+	}
+	for 1 < high-low {
+		mid := (low + high) / 2
+		if st[mid].Addr <= addr {
+			low = mid
+		} else {
+			high = mid
+		}
+	}
+	if st[low].Addr <= addr && addr < st[low].Addr+st[low].Size {
+		return st[low], true
+	}
+	return Symbol{}, false
+}
---
+++ symtab/lib_test.go
@@ -0,0 +1,82 @@
+package symtab_test
+
+import (
+	"testing"
+
+	"github.com/clkbug/konata-tools/symtab"
+)
+
+func testSetup() (symtab.SymbolTable, error) {
+	const filename = "coremark.symtab.txt"
+	return symtab.ParseSymbolTableFile(filename)
+}
+func TestParseSymbolTableFile(t *testing.T) {
+	st, err := testSetup()
+	if err != nil {
+		t.Error(err)
+	}
+	if len(st) != 107 {
+		t.Errorf("len(st), expected 107, but got %d", len(st))
+	}
+	f := 0
+	for _, x := range st {
+		if x.Type == symtab.SymFunc {
+			f++
+		}
+	}
+	if f != 43 {
+		t.Errorf("len(filter(fun x -> x.Type == SymFunc, st)) == 43, but got %d", f)
+	}
+}
+func TestSymbolTableSort(t *testing.T) {
+	st, err := testSetup()
+	if err != nil {
+		t.Error(err)
+	}
+	st.Sort()
+	var cur uint64
+	for _, x := range st {
+		if x.Addr < cur {
+			t.Errorf("Addr(%d) < %d (not sorted!)", x.Addr, cur)
+		}
+		cur = x.Addr
+	}
+}
+func TestSymbolTableSearch(t *testing.T) {
+	st, err := testSetup()
+	if err != nil {
+		t.Error(err)
+	}
+	st.Sort()
+	testTable := []struct {
+		addr     uint64
+		expected string
+	}{
+		{0x0000476c, "number"},
+		{0x0000476c + 1564/2, "number"},
+		{0x00001dac + 1508/3, "core_bench_list"},
+		{0x00004758, "portable_init"},
+		{0x00004758 + 1, "portable_init"},
+		{0x00004758 + 11, "portable_init"},
+	}
+	for _, x := range testTable {
+		s, ok := st.Search(x.addr)
+		if !ok {
+			t.Errorf("Search(%d) expected %s, but not found", x.addr, x.expected)
+		}
+		if s.Name != x.expected {
+			t.Errorf("Search(%d).Name expected %s, but got %s", x.addr, x.expected, s.Name)
+		}
+	}
+	notFounds := []uint64{
+		100,
+		200,
+		0xFFFFFFFF,
+	}
+	for _, x := range notFounds {
+		s, ok := st.Search(x)
+		if ok {
+			t.Errorf("Search(%d) should be not found, but got %v", x, s)
+		}
+	}
+}
